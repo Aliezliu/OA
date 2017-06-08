bdb83e16f4bd36546307e4fba84b04e23475d366
DESKTOP-ON0EP60\王致尧 <573636012@qq.com>
2017-06-08 20:04:50 +0800

    no message

--- OA/OA-mainsite/src/main/java/com/OA/mainsite/dao/UserMapper.java
+++
@@ -1,7 +0,0 @@
-package com.OA.mainsite.dao;
-
-import com.OA.mainsite.entity.User;
-
-public interface UserMapper {
-   User selectById(Integer id);
-}
--- OA/OA-mainsite/src/main/java/com/OA/mainsite/service/UserService.java
+++
@@ -1,8 +0,0 @@
-package com.OA.mainsite.service;
-
-import com.OA.mainsite.entity.User;
-
-public interface UserService {
-    
-	User getById(Integer id);
-}
